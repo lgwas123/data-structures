67967e776104793e16835c99b6b4fb8f85daec12
지우 이 <lgwas123@cu.ac.kr>
2020-10-22 12:49:09 +0900

    df

---
+++ ch05/ch05/5-5.cpp
@@ -0,0 +1,19 @@
+void recur(int n) {
+	IntStack stk;
+	Initialize(&stk, 100);
+
+Top :
+	if (n > 0) {
+		push(&stk, n);
+		n = n - 1
+			goto Top;
+	}
+	if (!IsEmpty(&stk)) {
+		Pop(&stk, &n);
+		printf("%d\n", n)
+			n = n - 2;
+		goto Top;
+	}
+
+	Termonare(&stk);
+}
