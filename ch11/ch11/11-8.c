6feda733ccb08e5993356ec7e1d7d0823f777f81
지우 이 <lgwas123@cu.ac.kr>
2020-12-14 18:14:47 +0900

    j

--- ch11/ch11/11-8.c
+++ ch11/ch11/11-8.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "Member.h"
-#include "ChainHash.h"
+#include "ClosedHash.h"
 
 typedef enum {
 	TERMINATE, ADD, DELETE, SEARCH, CLEAR, DUMP
@@ -10,7 +10,7 @@ Menu SelectMenu(void)
 {
 	int ch;
 	do {
-		printf("(1)추가 (2)삭제 (3)검색 (4)모두 삭제 (5)덤프 (0)종료 : ");
+		printf("(1)추가 (2)삭제 (3)검색 (4)모두삭제 (5)덤프 (0)종료 : ");
 		scanf_s("%d", &ch);
 	} while (ch < TERMINATE || ch > DUMP);
 	return(Menu)ch;
@@ -19,29 +19,36 @@ Menu SelectMenu(void)
 int main(void)
 {
 	Menu menu;
-	ChainHash hash;
+	ClosedHash hash;
 	Initialize(&hash, 13);
 	do {
 		int result;
 		Member x;
-		Node* temp;
+		Bucket* temp;
 		switch (menu = SelectMenu()) {
 		case ADD :
 			x = ScanMember("추가", MEMBER_NO | MEMBER_NAME);
 			result = Add(&hash, &x);
 			if (result)
 				printf("\a오류 : 추가에 실패했습니다(%s).\n",
-					(result == 1) ? "이미 등록됨" : "메모리 부족");
+					(result == 1) ? "등록마침" : "메모리 부족");
 			break;
 
-		case DELETE :
+		case DELETE : 
 			x = ScanMember("삭제", MEMBER_NO);
+			result = Remove(&hash, &x);
+			if (result == 1)
+				printf("\a오류 : 이 번호의 데이터는 존재하지 않습니다.\n");
+			break;
+
+		case SEARCH :
+			x = ScanMember("검색", MEMBER_NO);
 			temp = Search(&hash, &x);
 			if (temp == NULL)
 				printf("\a검색에 실패했습니다.\n");
 			else {
 				printf("검색에 성공했습니다. : ");
-				
+				PrintLnMember(&temp->data);
 			}
 			break;
 
@@ -54,6 +61,7 @@ int main(void)
 			break;
 		}
 	} while (menu != TERMINATE);
+
 	Terminate(&hash);
 
 	return 0;
